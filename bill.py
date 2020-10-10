9d92079d6c0cd1c2f14d382b03e02bdb3d2a92b3
athithan176005H <72656537+athithan176005H@users.noreply.github.com>
2020-10-10 18:18:39 +0530

    total bill

---
+++ bill.py
@@ -0,0 +1,10 @@
+print("BILL")
+print('......................................................................')
+D=int(input('Enter price 1:'))
+E=int(input('Enter price 2:'))
+F=int(input('Enter price 3:'))
+G=int(input('Enter price 4:'))
+Bill=(D+E+F+G)
+print("Total: "),Bill
+
+
